93983cb98ec1f3481f6b7b76e24730ea2191c4b3
01fade <01fade@users.noreply.github.com>
2020-06-17 17:00:42 -0700

    test jetpack raw function reset

--- themes/interconnection/single.php
+++ themes/interconnection/single.php
@@ -16,61 +16,13 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'template-parts/content', get_post_type() );
-
-				$related = Jetpack_RelatedPosts::init_raw()->get_for_post_id( get_the_ID(), array( 'size' => 3 ) );
-
-				if ( $related ) {
-					foreach ( $related as $result ) {
-						$posts_id[] = $result[ 'id' ];
-					}
-
-					$the_query = new WP_Query( array(
-						'post_type' => 'post',
-						'post__in' => $posts_id
-					) );
-
-					if( $the_query->have_posts() ) {
-					while ( $the_query->have_posts() ) { $the_query->the_post();
-						// get_template_part( 'template-parts/similar-post' );
-						echo 'title' . get_the_title();
-						}
-					}
-
-					wp_reset_query();
-				}
 			?>
 
 			<div class="wrapper">
-				<?php echo do_shortcode( '[jetpack-related-posts]' ); ?>
 
-				<?php if ( class_exists( 'Jetpack_RelatedPosts' ) && method_exists( 'Jetpack_RelatedPosts', 'init_raw' ) ) { ?>
+				<?php if ( class_exists( 'Jetpack_RelatedPosts' ) ) { ?>
 					<div class="jetpack-related-posts">
-						<?php 
-							$related = Jetpack_RelatedPosts::init_raw()->get_for_post_id( get_the_ID(), array( 'size' => 3 ) );
-							echo get_the_ID();
-							var_dump($related);
-
-							if ( $related ) {
-								foreach ( $related as $result ) {
-									$posts_id[] = $result[ 'id' ];
-								}
-
-								$the_query = new WP_Query( array(
-									'post_type' => 'post',
-									'post__in' => $posts_id
-								) );
-
-								if( $the_query->have_posts() ) {
-								while ( $the_query->have_posts() ) { $the_query->the_post();
-									// get_template_part( 'template-parts/similar-post' );
-									echo get_the_title();
-									}
-								}
-
-								wp_reset_query();
-							}
-
-						?>
+						<?php echo do_shortcode( '[jetpack-related-posts]' ); ?>
 					</div>
 				<?php } else {
 					the_post_navigation(
