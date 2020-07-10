6d8ec289405314820105728c1f8be41b7a3ae8f2
Ckoerner <nobelx@gmail.com>
2020-07-10 16:44:12 -0500

    Found the culprit. Readding plugins without issues to activate in code.

--- client-mu-plugins/plugin-loader.php
+++ client-mu-plugins/plugin-loader.php
@@ -18,20 +18,20 @@
  * wpcom_vip_load_plugin only loads plugins from the `WP_PLUGIN_DIR` directory.
  * For client-mu-plugins `require __DIR__ . '/plugin-name/plugin-name.php'` works.
  */
-//wpcom_vip_load_plugin( 'amp' );
+wpcom_vip_load_plugin( 'amp' );
 //wpcom_vip_load_plugin('publishpress');
-//wpcom_vip_load_plugin('capability-manager-enhanced/capsman-enhanced.php');
+wpcom_vip_load_plugin('capability-manager-enhanced/capsman-enhanced.php');
 //wpcom_vip_load_plugin('miniorange-oauth-oidc-single-sign-on/mo_oauth_settings.php');
-//wpcom_vip_load_plugin('polylang/polylang.php');
-//wpcom_vip_load_plugin('wpdiscuz/class.WpdiscuzCore.php');
-//wpcom_vip_load_plugin('wpdiscuz-comment-search/wpDiscuzCommentSearch.php');
-//wpcom_vip_load_plugin('wpdiscuz-comment-translation/wpdiscuz-translate.php');
-//wpcom_vip_load_plugin('wpdiscuz-frontend-moderation/class.wpDiscuzFrontEndModeration.php');
-//wpcom_vip_load_plugin('wpdiscuz-report-flagging/wpDiscuzFlagComment.php');
-//wpcom_vip_load_plugin('wpdiscuz-subscribe-manager/wpdSubscribeManager.php');
-//wpcom_vip_load_plugin('wpdiscuz-syntax-highlighter/wpDiscuzSyntaxHighlighter.php');
-//wpcom_vip_load_plugin('wpdiscuz-user-comment-mentioning/WpdiscuzUCM.php');
-//wpcom_vip_load_plugin('wpdiscuz-widgets/wpDiscuzWidgets.php');
-//wpcom_vip_load_plugin('co-authors-plus/co-authors-plus.php');
-//wpcom_vip_load_plugin('fieldmanager/fieldmanager.php');
-//wpcom_vip_load_plugin('diff-customizations/diff-customizations.php');
+wpcom_vip_load_plugin('polylang/polylang.php');
+wpcom_vip_load_plugin('wpdiscuz/class.WpdiscuzCore.php');
+wpcom_vip_load_plugin('wpdiscuz-comment-search/wpDiscuzCommentSearch.php');
+wpcom_vip_load_plugin('wpdiscuz-comment-translation/wpdiscuz-translate.php');
+wpcom_vip_load_plugin('wpdiscuz-frontend-moderation/class.wpDiscuzFrontEndModeration.php');
+wpcom_vip_load_plugin('wpdiscuz-report-flagging/wpDiscuzFlagComment.php');
+wpcom_vip_load_plugin('wpdiscuz-subscribe-manager/wpdSubscribeManager.php');
+wpcom_vip_load_plugin('wpdiscuz-syntax-highlighter/wpDiscuzSyntaxHighlighter.php');
+wpcom_vip_load_plugin('wpdiscuz-user-comment-mentioning/WpdiscuzUCM.php');
+wpcom_vip_load_plugin('wpdiscuz-widgets/wpDiscuzWidgets.php');
+wpcom_vip_load_plugin('co-authors-plus/co-authors-plus.php');
+wpcom_vip_load_plugin('fieldmanager/fieldmanager.php');
+wpcom_vip_load_plugin('diff-customizations/diff-customizations.php');
